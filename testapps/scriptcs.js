0d7676392a1fa0aef87dd5e072ed1e2e4e685b77
haas-do <dominik.haas0903@gmail.com>
2026-08-12 11:29:01 +0200

    Cesium-Anwendung und Testapps hinzugefügt

---
+++ testapps/scriptcs.js
@@ -0,0 +1,136 @@
+// Zeitbereich
+const minUnix = -4244572800000;   // 1835-07-01
+const maxUnix = 1772319600000;  // 2026-03-01
+
+const myslider = document.getElementById("time-slider");
+const label = document.getElementById("time-label");
+
+
+// Slider konfigurieren
+myslider.min = minUnix;
+myslider.max = maxUnix;
+myslider.value = minUnix;//Standard: minUnix
+
+// Startdatum
+let currentDate = new Date(minUnix);//Standard: minUnix
+const unixTime = currentDate.getTime();
+
+// Anzeige aktualisieren
+function updateLabel() {
+  label.textContent = currentDate.toLocaleDateString("de-DE");
+}
+
+
+
+myslider.addEventListener("input", () => {
+  const unixTime = parseInt(myslider.value);
+
+  currentDate = new Date(unixTime);
+
+  updateLabel(); // sofort
+  updateVisibility();
+  //updateMap();   // sofort
+});
+
+// Buttons
+document.querySelectorAll("#time-control button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const step = parseInt(btn.dataset.step);
+    const unit = btn.dataset.unit;
+
+    if (unit === "day") currentDate.setDate(currentDate.getDate() + step);
+    if (unit === "month") currentDate.setMonth(currentDate.getMonth() + step);
+    if (unit === "year") currentDate.setFullYear(currentDate.getFullYear() + step);
+
+    let unix = currentDate.getTime();
+    
+    // Begrenzen
+    unix = Math.max(minUnix, Math.min(maxUnix, unix));
+
+    currentDate = new Date(unix);
+    myslider.value = unix;
+
+    updateLabel();
+    updateVisibility();
+  });
+});
+
+// Initial
+updateLabel();
+
+const provider = new Cesium.UrlTemplateImageryProvider({
+    url: 'https://wmtsod1.bayernwolke.de/wmts/by_webkarte_grau/smerc/{z}/{x}/{y}',
+    minimumLevel: 0,
+    maximumLevel: 19,
+    credit: 'Bayerische Vermessungsverwaltung - www.geodaten.bayern.de (Daten verändert)'
+  });
+
+  var viewer = new Cesium.Viewer("cesiumContainer", {
+    imageryProvider : provider,
+    baseLayerPicker: false,
+    animation : false,
+    timeline: false
+  });
+
+  //Dein lokales Tileset
+  const tileset = new Cesium.Cesium3DTileset({
+    url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-11-3857/tileset.json",
+    color:'red'
+  });
+
+  viewer.scene.primitives.add(tileset);
+
+  const loadedTiles = [];
+
+  viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(11.69573, 49.57710, 860),
+    orientation: {
+      heading: Cesium.Math.toRadians(0),//Zeigt nach Norden
+      pitch: Cesium.Math.toRadians(-90),//Neigung
+      roll: 0
+    },
+    duration: 0
+  });
+
+//==================== 
+//Zeitlogik
+//====================
+
+    tileset.tileLoad.addEventListener(function(tile) {
+        const content = tile.content;
+        if (!Cesium.defined(content) || !content.featuresLength)
+            return;
+        loadedTiles.push(tile);
+        for (let i = 0; i < content.featuresLength; i++) {
+            const feature = content.getFeature(i);
+            feature._e_epoche = Number(feature.getProperty("e_epoche"));
+            feature._u_epoche = Number(feature.getProperty("u_epoche"));
+        }
+        updateVisibility();
+    });
+
+    function updateVisibility() {
+    const currentUnix = currentDate.getTime();
+    loadedTiles.forEach(tile => {
+
+        const content = tile.content;
+        if (!Cesium.defined(content) || !content.featuresLength)
+            return;
+        for (let i = 0; i < content.featuresLength; i++) {
+            const feature = content.getFeature(i);
+            feature.show =
+                currentUnix >= feature._e_epoche &&
+                currentUnix <= feature._u_epoche;
+            }
+        });
+    };
+
+    const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+    handler.setInputAction(function(click){
+        const feature = viewer.scene.pick(click.position);
+        console.log(feature);
+        if(Cesium.defined(feature)){
+            console.log(feature.getProperty);
+        }
+
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
---
+++ testapps/scriptml.js
@@ -0,0 +1,187 @@
+//========================================
+//Zeitsteuerung
+//========================================    
+
+// Zeitbereich des Sliders
+const minUnix = -4244572800000;   // 1835-07-01
+const maxUnix = 1772319600000;  // 2026-03-01
+
+const slider = document.getElementById("time-slider");
+const label = document.getElementById("time-label");
+
+// Slider konfigurieren
+slider.min = minUnix;
+slider.max = maxUnix;
+slider.value = minUnix;//Standard: minUnix
+
+// Startdatum
+let currentDate = new Date(minUnix);//Standard: minUnix
+
+// Anzeige aktualisieren
+function updateLabel() {
+  label.textContent = currentDate.toLocaleDateString("de-DE");
+}
+
+// Definition des Zeitfilter
+function buildTimeFilter(unixTime) {
+  return [
+    "all",
+    ["<=", ["get", "e_epoche"], unixTime],
+    [
+      "any",
+      [">=", ["get", "u_epoche"], unixTime],
+      ["!", ["has", "u_epoche"]]
+    ]
+  ];
+}
+
+// Anwendung des Zeitfilters und Deklarierung aller sonstigen Filter
+function updateMap() {
+  const unixTime = currentDate.getTime();
+  const timefilter = buildTimeFilter(unixTime);
+
+const layers = [
+  ""
+];
+
+layers.forEach(layer => {
+    if (map.getLayer(layer)) {
+      map.setFilter(layer, timefilter);
+    }
+  });
+
+  map.setFilter("zr_Gebaeude3D_nicht_oeffentlich", ["all",timefilter,["match",["get", "klasse"],["DTK25-NDFK","Gebäude für Wirtschaft oder Gewerbe","Wohngebäude","Überdachung","Garage","Nach Quellenlage nicht zu spezifizieren","Umformer","Wehr","Wasserbehälter","Historische Mauer","Kaserne","Tiefgarage","Parkhaus","Staumauer","Kammerschleuse"],true,false]]);
+  map.setFilter("zr_Gebaeude3D_oeffentlich", ["all",timefilter,["match",["get", "klasse"],["DTK25-NDFK","Gebäude für Wirtschaft oder Gewerbe","Wohngebäude","Überdachung","Garage","Nach Quellenlage nicht zu spezifizieren","Umformer","Brücke","Wehr","Wasserbehälter","Historische Mauer","Kaserne","Tiefgarage","Parkhaus","Staumauer","Kammerschleuse"],false,true]]);
+}
+
+//Update des Datums bei Veränderung des Sliders
+slider.addEventListener("input", () => {
+  const unixTime = parseInt(slider.value);
+
+  currentDate = new Date(unixTime);
+
+  updateLabel();
+  updateMap();
+});
+
+// Definiton der Buttons der Zeitsteurung
+document.querySelectorAll("#time-control button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const step = parseInt(btn.dataset.step);
+    const unit = btn.dataset.unit;
+
+    if (unit === "day") currentDate.setDate(currentDate.getDate() + step);
+    if (unit === "month") currentDate.setMonth(currentDate.getMonth() + step);
+    if (unit === "year") currentDate.setFullYear(currentDate.getFullYear() + step);
+
+    let unix = currentDate.getTime();
+    
+    // Begrenzung der Funktionalität innerhalb des Zeitintervalls
+    unix = Math.max(minUnix, Math.min(maxUnix, unix));
+
+    currentDate = new Date(unix);
+    slider.value = unix;
+
+    updateLabel();
+    updateMap();
+  });
+});
+
+// Initiales Update der Zeitanzeige
+updateLabel();
+
+//========================================
+//Karteninhalt
+//========================================
+//Initialisierung und Variablendeklarierung zur Verwendung von PMTiles
+const protocol = new pmtiles.Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
+
+const p = new pmtiles.PMTiles("zeitreise_3857.pmtiles");
+protocol.add(p);
+
+//Laden der Kartenquellen und Layer
+const map = new maplibregl.Map({
+        container: 'map',
+        style: 'https://vtod1.bayernwolke.de/styles/by_style_light.json',
+        hash:true,
+        zoom: 16,
+        center: [11.69573, 49.57710],
+        minZoom: 12,           // Minimaler Zoom
+        maxZoom: 20,          // Maximaler Zoom
+        maxBounds: [
+            [11.55177, 49.48522], // Südwestliche räumliche Begrenzung der Karte
+            [11.83918, 49.67145] // Nordöstliche Begrenzung der Karte
+        ]
+    });
+
+    //Festlegen der Steuerungsoptionen
+    map.addControl(new maplibregl.NavigationControl({
+        visualizePitch: true,
+        visualizeRoll: true,
+        showZoom: true,
+        showCompass: true
+    }));
+
+    //Kartentitel
+    // Ausblenden des Titels nach 5 Sekunden
+    function hideTitle() {
+    document.getElementById("mapTitle").classList.add("hide");
+    }
+
+    map.once("dragstart", hideTitle);
+    map.once("zoomstart", hideTitle);
+    map.once("rotatestart", hideTitle);
+
+    setTimeout(hideTitle, 5000);
+
+
+    map.on('load', () => {
+        map.addSource('zr', {
+            type: 'vector',
+            url: "pmtiles://https://haas-do.github.io/zeitreise-edelsfeld/data/pmtiles-3857/zeitreise_3857.pmtiles"
+        }
+    );    
+    
+        map.addLayer({
+            'id': 'zr_Gebaeude3D_nicht_oeffentlich',
+            'type': 'fill-extrusion',
+            'source': 'zr',
+            'source-layer': 'Gebaeudeflaeche',
+            "minzoom": 14,
+            "paint": {
+            "fill-extrusion-color": "rgb(179,179,179)",
+            "fill-extrusion-height": [
+            "get",
+            "hoehe"
+            ],
+            "fill-extrusion-opacity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                14,
+                0,
+                14.5,
+                0.7
+                ]
+        }
+        });
+
+        
+        map.addLayer({
+            'id': 'zr_Gebaeude3D_oeffentlich',
+            'type': 'fill-extrusion',
+            'source': 'zr',
+            'source-layer': 'Gebaeudeflaeche',
+            "minzoom": 14.5,
+            "paint": {
+            "fill-extrusion-color": "rgb(193,160,161)",
+            "fill-extrusion-height": [
+            "get",
+            "hoehe"
+            ],
+            "fill-extrusion-opacity": 0.7
+        }
+        });
+    updateMap();
+    });
