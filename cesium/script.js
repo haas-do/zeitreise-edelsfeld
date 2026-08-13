f707e4e031e80b0bf648f9aca0a9435ee1673d2a
haas-do <dominik.haas0903@gmail.com>
2026-08-13 13:55:01 +0200

    update cesium

--- cesium/script.js
+++ cesium/script.js
@@ -59,7 +59,7 @@ document.querySelectorAll("#time-control button").forEach(btn => {
 updateLabel();
 
 const provider = new Cesium.UrlTemplateImageryProvider({
-    url: 'https://wmtsod1.bayernwolke.de/wmts/by_webkarte_grau/smerc/{z}/{x}/{y}',
+    url: 'https://wmtsod1.bayernwolke.de/wmts/by_webkarte/smerc/{z}/{x}/{y}',
     //url: 'https://wmtsod1.bayernwolke.de/wmts/by_dop/smerc/{z}/{x}/{y}',
     minimumLevel: 0,
     maximumLevel: 19,
@@ -76,59 +76,155 @@ const provider = new Cesium.UrlTemplateImageryProvider({
     timeline: false
   });
 
-  //Dein lokales Tileset
-  const tileset = new Cesium.Cesium3DTileset({
-    //url: "http://localhost:5500/output/tileset.json"
-    url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-11-3857/tileset.json",
-    color:'red'
-  });
+  //   url: "",
+  //   url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/hintergrund2/tileset.json",
+  //   url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/vegetationsflaeche/tileset.json",
+  //   url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/verkehrslinie/tileset.json",
 
-  viewer.scene.primitives.add(tileset);
+  const tilesets = [
+    new Cesium.Cesium3DTileset({
+        url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-11-3857/tileset.json"
+    }),
 
-  const loadedTiles = [];
+    new Cesium.Cesium3DTileset({
+        url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/gewaesserflaeche/tileset.json"
+    }),
+
+    new Cesium.Cesium3DTileset({
+        url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/siedlungsflaeche/tileset.json"
+    }),
+
+    new Cesium.Cesium3DTileset({
+        url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/vegetationsflaeche/tileset.json",
+    }),
+
+    new Cesium.Cesium3DTileset({
+        url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/hintergrund/tileset.json"
+    }),
+
+    new Cesium.Cesium3DTileset({
+        url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/hintergrund2/tileset.json"
+    }),
+
+        new Cesium.Cesium3DTileset({
+        url: "https://haas-do.github.io/zeitreise-edelsfeld/data/3dtiles-hintergrund/verkehrslinie/tileset.json"
+    })
+  ];
+
+  // Gebäude Tileset  
+  var cityStyle = new Cesium.Cesium3DTileStyle({
+        color : {
+            conditions : [
+                ["${klasse} === 'Wohngebäude'", "color('rgb(179, 179, 179)')"],
+                ["${klasse} === 'Gebäude für Wirtschaft und Gewerbe'", "color('rgb(179, 179, 179)')"],
+                ["${klasse} === 'Gebäude für Wirtschaft oder Gewerbe'", "color('rgb(179, 179, 179)')"],
+                ["${klasse} === 'Garage'", "color('rgb(179, 179, 179)')"],
+                ["${klasse} === 'Kirche'", "color('rgb(193, 160, 161)')"],
+                ["${klasse} === 'Rathaus'", "color('rgb(193, 160, 161)')"],
+                ["${klasse} === 'Feuerwehr'", "color('rgb(193, 160, 161)')"],
+                ["${klasse} === 'Kinderkrippe, Kindergarten, Kindertagesstätte'", "color('rgb(193, 160, 161)')"],
+                ["${klasse} === 'Gebäude für Bildung und Forschung'", "color('rgb(193, 160, 161)')"],
+                ["${klasse} === 'See'", "color('#A5BEEB')"],
+                ["${klasse} === 'Rückhaltebecken'", "color('#A5BEEB')"],
+                ["${klasse} === 'Siedlung'", "color('rgb(240, 230, 239)')"],
+                ["${klasse} === 'Industrie- und Gewerbefläche'", "color('rgb(221,218,226)')"],
+                ["${klasse} === 'Nadelholz'", "color('rgb(154,182,109)')"],
+                ["${klasse} === 'Grünland'", "color('rgb(244, 250, 229)')"],
+            ]
+        }
+    });
+    tilesets.style = cityStyle;
+
+    tilesets.forEach(tileset => {tileset.style = cityStyle;});
+
+    //tilesets.imageBasedLighting.imageBasedLightingFactor = new Cesium.Cartesian2(13.0, 13.0);
+
 
   viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(11.69573, 49.57710, 860), 
     orientation: {
       heading: Cesium.Math.toRadians(0),//Zeigt nach Norden
-      pitch: Cesium.Math.toRadians(-45),//Neigung
+      pitch: Cesium.Math.toRadians(-90),//Neigung
       roll: 0
     },
     duration: 0
   });
 
-//==================== 
-//Zeitlogik
-//====================
+// Alle Tilesets zu Cesium hinzufügen
+tilesets.forEach(tileset => {
+    viewer.scene.primitives.add(tileset);
+});
+
+
+// ============================================================
+// GELADENE TILES
+// ============================================================
+
+// Hier werden alle geladenen Tiles aus allen Tilesets gespeichert
+const loadedTiles = [];
+
+
+// ============================================================
+// TILE LOAD FÜR ALLE TILESETS
+// ============================================================
+
+tilesets.forEach(tileset => {
 
     tileset.tileLoad.addEventListener(function(tile) {
+
         const content = tile.content;
-        if (!Cesium.defined(content) || !content.featuresLength)
+
+        if (!Cesium.defined(content) || !content.featuresLength) {
             return;
-        loadedTiles.push(tile);
-        for (let i = 0; i < content.featuresLength; i++) {
-            const feature = content.getFeature(i);
-            feature._e_epoche = Number(feature.getProperty("e_epoche"));
-            feature._u_epoche = Number(feature.getProperty("u_epoche"));
         }
+
+        // Tile speichern
+        loadedTiles.push(tile);
+
+        // Attribute aller Features auslesen
+        for (let i = 0; i < content.featuresLength; i++) {
+
+            const feature = content.getFeature(i);
+
+            feature._e_epoche =
+                Number(feature.getProperty("e_epoche"));
+
+            feature._u_epoche =
+                Number(feature.getProperty("u_epoche"));
+        }
+
+        // Zeitfilter anwenden
         updateVisibility();
     });
+});
 
-    function updateVisibility() {
+
+// ============================================================
+// SICHTBARKEIT AKTUALISIEREN
+// ============================================================
+
+function updateVisibility() {
+
     const currentUnix = currentDate.getTime();
+
     loadedTiles.forEach(tile => {
 
         const content = tile.content;
-        if (!Cesium.defined(content) || !content.featuresLength)
+
+        if (!Cesium.defined(content) || !content.featuresLength) {
             return;
+        }
+
         for (let i = 0; i < content.featuresLength; i++) {
+
             const feature = content.getFeature(i);
+
             feature.show =
                 currentUnix >= feature._e_epoche &&
                 currentUnix <= feature._u_epoche;
-            }
-        });
-    };
+        }
+    });
+}
 
     const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
     handler.setInputAction(function(click){
